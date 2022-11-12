70d96fcff21226e675eb21ea6caa1b8d77fdb0f9
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2022-11-12 21:55:02 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use bracket_lib::prelude::*;
+use prelude::{Map, Player, SCREEN_HEIGHT, SCREEN_WIDTH};
+
+mod map;
+mod player;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+    pub use crate::player::*;
+}
+
+struct State {
+    map: Map,
+    player: Player,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            map: Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut bracket_lib::terminal::BTerm) {
+        ctx.cls();
+        self.player.update(ctx, &self.map);
+        self.map.render(ctx);
+        self.player.render(ctx);
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(context, State::new())
+}
